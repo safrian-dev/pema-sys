81ba447df18b2b11256192a068f3bb5f6a00f0a0
unknown <hello.safrian@gmail.com>
2023-12-20 16:44:45 +0700

    'update'

---
+++ static/js/747.70659b3a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaterialpro_main=self.webpackChunkmaterialpro_main||[]).push([[747],{22747:(a,s,t)=>{t.r(s),t.d(s,{default:()=>c});t(72791);var e=t(74149),r=(t(84343),t(80184));const c=()=>(0,r.jsx)(e.Z,{src:"https://www.jotform.com/tables/233040333516040"})},84343:()=>{}}]);
+//# sourceMappingURL=747.70659b3a.chunk.js.map
---
+++ static/js/761.8efe31c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaterialpro_main=self.webpackChunkmaterialpro_main||[]).push([[761],{32761:(s,a,e)=>{e.r(a),e.d(a,{default:()=>i});e(72791);var r=e(74149),c=e(63009),m=(e(99698),e(80184));const i=()=>(0,m.jsx)(c.Z,{children:(0,m.jsxs)("div",{className:"form-conteiner",children:[(0,m.jsx)("div",{className:"form-content",children:(0,m.jsx)(r.Z,{src:"https://form.jotform.com/233040333516040"})}),(0,m.jsx)("div",{className:"mask"})]})})},99698:()=>{}}]);
+//# sourceMappingURL=761.8efe31c3.chunk.js.map
